eaabcc5927f1061148effdb4f37984a57e773bb5
Rich Banyi <richardbanyi@Richards-MacBook-Pro.local>
2015-05-19 11:41:01 +0200

    Correct namings

--- models.py
+++ models.py
@@ -8,54 +8,54 @@ db = SQLAlchemy(app)
 class Airline(db.Model):
     __tablename__ = 'airline'
 
-    airlineid = db.Column(db.Integer, primary_key=True)
-    airlineode = db.Column(db.String)
-    airlinename = db.Column(db.String)
+    airline_id = db.Column(db.Integer, primary_key=True)
+    airline_code = db.Column(db.String)
+    airline_name = db.Column(db.String)
 
-    def __init__(self, airlinecode, airlinename):
-        self.airlinecode = airlinecode
-        self.airlinename = airlinename
+    def __init__(self, airline_code, airline_name):
+        self.airline_code = airline_code
+        self.airline_name = airline_name
 
 class Airport(db.Model):
     __tablename__ = 'airport'
 
-    airportid = db.Column(db.Integer, primary_key=True)
-    airlineid = db.Column(db.Integer, db.ForeignKey('airline.airlineid'))
-    airline = db.relationship('Airline', backref=db.backref('airport', order_by=airportid))
-    airportcode = db.Column(db.String)
+    airport_id = db.Column(db.Integer, primary_key=True)
+    airline_id = db.Column(db.Integer, db.ForeignKey('airline.airline_id'))
+    airline = db.relationship('Airline', backref=db.backref('airport', order_by=airport_id))
+    airport_code = db.Column(db.String)
     city = db.Column(db.String)
     state = db.Column(db.String)
     
-    def __init__(self, airporcode, city, state):
-        self.airportcode = airportcode
+    def __init__(self, airport_code, city, state):
+        self.airport_code = airport_code
         self.city = city
         self.state = state
 
 class Aircraft(db.Model):
     __tablename__ = 'aircraft'
 
-    aircraftid = db.Column(db.Integer, primary_key=True)
-    airlineid = db.Column(db.Integer, db.ForeignKey('airline.airlineid'))
-    airline = db.relationship('Airline', backref=db.backref('aircraft', order_by=aircraftid))
-    aircrafttype = db.Column(db.String)
+    aircraft_id = db.Column(db.Integer, primary_key=True)
+    airline_id = db.Column(db.Integer, db.ForeignKey('airline.airline_id'))
+    airline = db.relationship('Airline', backref=db.backref('aircraft', order_by=aircraft_id))
+    aircraft_type = db.Column(db.String)
     capacity = db.Column(db.Integer)
 
-    def __init__(self, aircrafttype, capacity):
-        self.aircrafttype = aircrafttype
+    def __init__(self, aircraft_type, capacity):
+        self.aircraft_type = aircraft_type
         self.capacity = capacity
 
 class Flight(db.Model):
     __tablename__ = 'flight'
 
-    flightid = db.Column(db.Integer, primary_key=True)
-    airlineid = db.Column(db.Integer, db.ForeignKey('airline.airlineid'))
-    airline = db.relationship('Airline', backref=db.backref('flight', order_by=flightid))
-    aircraftid = db.Column(db.Integer, db.ForeignKey('aircraft.aircraftid'))
-    aircraft = db.relationship('Aircraft', backref=db.backref('flight', order_by=flightid))
-    fromdestination = db.Column(db.Integer, db.ForeignKey('airport.airportid'))
-    todestination = db.Column(db.Integer, db.ForeignKey('airport.airportid'))
-    fromdest = db.relationship('Airport',backref=db.backref('fromDest', order_by=flightid), foreign_keys=[fromdestination])
-    todest = db.relationship('Airport',backref=db.backref('toDest', order_by=flightid), foreign_keys=[todestination])
+    flight_id = db.Column(db.Integer, primary_key=True)
+    airline_id = db.Column(db.Integer, db.ForeignKey('airline.airline_id'))
+    airline = db.relationship('Airline', backref=db.backref('flight', order_by=flight_id))
+    aircraft_id = db.Column(db.Integer, db.ForeignKey('aircraft.aircraft_id'))
+    aircraft = db.relationship('Aircraft', backref=db.backref('flight', order_by=flight_id))
+    fromdestination = db.Column(db.Integer, db.ForeignKey('airport.airport_id'))
+    todestination = db.Column(db.Integer, db.ForeignKey('airport.airport_id'))
+    fromdest = db.relationship('Airport',backref=db.backref('fromDest', order_by=flight_id), foreign_keys=[fromdestination])
+    todest = db.relationship('Airport',backref=db.backref('toDest', order_by=flight_id), foreign_keys=[todestination])
     departuredate = db.Column(db.DateTime)
     departuretime = db.Column(db.DateTime)
     arrivaldate = db.Column(db.DateTime)
@@ -70,73 +70,73 @@ class Flight(db.Model):
 class UserProfile(db.Model):
     __tablename__ = 'userprofile'
 
-    userid = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String)
-    lastname = db.Column(db.String)
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
     email = db.Column(db.String)
     phone = db.Column(db.String)
     street = db.Column(db.String)
-    streetnumber = db.Column(db.Integer)
+    street_number = db.Column(db.Integer)
     city = db.Column(db.String)
-    zipcode = db.Column(db.String)
+    zip_code = db.Column(db.String)
 
-    def __init__(self, firstname, lastname, email, phone, street, streetnumber, city, zipcode):
-        self.firstname = firstname
-        self.lastname = lastname
+    def __init__(self, first_name, last_name, email, phone, street, street_number, city, zip_code):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.phone = phone
         self.street = street
-        self.streetnumber = streetnumber
+        self.street_number = street_number
         self.city = city
-        self.zipcode = zipcode
+        self.zip_code = zip_code
 
 class Account(db.Model):
     __tablename__ = 'account'
 
-    accountid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('userprofile.userid'))
-    user = db.relationship('UserProfile', backref=db.backref('account', order_by=accountid))
-    usertype = db.Column(db.String)
-    loginname = db.Column(db.String)
+    account_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('userprofile.user_id'))
+    user = db.relationship('UserProfile', backref=db.backref('account', order_by=account_id))
+    user_type = db.Column(db.String)
+    login_name = db.Column(db.String)
     password = db.Column(db.String)
 
-    def __init__(self, usertype, loginname, password):
-        self.usertype = usertype
-        self.loginname = loginname
+    def __init__(self, user_type, login_name, password):
+        self.user_type = user_type
+        self.login_name = login_name
         self.password = password
 
 class CreditCard(db.Model):
     __tablename__ = 'creditcard'
 
-    cardid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('userprofile.userid'))
-    user = db.relationship('UserProfile', backref=db.backref('creditcard', order_by=cardid))
-    cardtype = db.Column(db.String)
-    cardnumber = db.Column(db.Integer)
-    expirationmonth = db.Column(db.Integer)
-    expirationyear = db.Column(db.Integer)
+    card_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('userprofile.user_id'))
+    user = db.relationship('UserProfile', backref=db.backref('creditcard', order_by=card_id))
+    card_type = db.Column(db.String)
+    card_number = db.Column(db.Integer)
+    expiration_month = db.Column(db.Integer)
+    expiration_year = db.Column(db.Integer)
 
-    def __init__(self, cardtype, cardnumber, expirationmonth, expirationyear):
-        self.cardtype = cardtype
-        self.cardnumber = cardnumber
-        self.expirationmonth = expirationmonth
-        self.expirationyear = expirationyear
+    def __init__(self, card_type, card_number, expiration_month, expiration_year):
+        self.card_type = card_type
+        self.card_number = card_number
+        self.expiration_month = expiration_month
+        self.expiration_year = expiration_year
 
 class Booking(db.Model):
     __tablename__ = 'booking'
 
-    bookid = db.Column(db.Integer, primary_key=True)
-    flightid = db.Column(db.Integer, db.ForeignKey('flight.flightid'))
-    flight = db.relationship('Flight', backref=db.backref('booking', order_by=bookid))
-    userid = db.Column(db.Integer, db.ForeignKey('userprofile.userid'))
-    user = db.relationship('UserProfile', backref=db.backref('booking', order_by=bookid))
-    bookdate = db.Column(db.DateTime)
+    book_id = db.Column(db.Integer, primary_key=True)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flight.flight_id'))
+    flight = db.relationship('Flight', backref=db.backref('booking', order_by=book_id))
+    user_id = db.Column(db.Integer, db.ForeignKey('userprofile.user_id'))
+    user = db.relationship('UserProfile', backref=db.backref('booking', order_by=book_id))
+    book_date = db.Column(db.DateTime)
     seats = db.Column(db.String)
     class_ = db.Column(db.String)
     price = db.Column(db.Float)
 
-    def __init__(self, bookdate, seats, class_, price):
-        self.bookdate = bookdate
+    def __init__(self, book_date, seats, class_, price):
+        self.book_date = book_date
         self.seats = seats
         self.class_ = class_
         self.price = price
