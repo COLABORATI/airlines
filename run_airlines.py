77e6dbb07317b054ebf2d028f7be248d68dec012
Rich Banyi <richardbanyi@Richards-MacBook-Pro.local>
2015-05-20 00:04:16 +0200

    CRUD for other entities

--- run_airlines.py
+++ run_airlines.py
@@ -7,9 +7,21 @@ from flask_sqlalchemy import SQLAlchemy
 def show_all():
     return render_template('show_all.html', flights=Flight.query.order_by(Flight.flight_id.desc()).all())
 
-@app.route('/show_flight/<flight_id>', methods=['GET', 'POST'])
-def show_flight(flight_id):
-    return render_template('show_flight.html', flight_=Flight.query.filter(Flight.flight_id==flight_id))
+@app.route('/show_flight', methods=['GET', 'POST'])
+def show_flight():
+    return render_template('show_flight.html', flights=Flight.query.order_by(Flight.flight_id.desc()).all())
+
+@app.route('/show_airport', methods=['GET', 'POST'])
+def show_airport():
+    return render_template('show_airport.html', airports=Airport.query.order_by(Airport.airport_id.desc()).all())
+
+@app.route('/show_airline', methods=['GET', 'POST'])
+def show_airline():
+    return render_template('show_airline.html', airlines=Airline.query.order_by(Airline.airline_id.desc()).all())
+
+@app.route('/show_aircraft', methods=['GET', 'POST'])
+def show_aircraft():
+    return render_template('show_aircraft.html', aircrafts=Aircraft.query.order_by(Aircraft.aircraft_id.desc()).all())
 
 @app.route('/search_flight/<flight_num>', methods=['GET', 'POST'])
 def search_flight(flight_num):
@@ -25,6 +37,9 @@ def search_flight(flight_num):
     #if form.validate_on_submit():
         #return redirect(url_for('show_all'))
     #return render_template('search.html', form=form.search.data)
+
+"""=================== NEW ==================="""
+
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
@@ -57,6 +72,75 @@ def new():
             return redirect(url_for('show_all'))
     return render_template('new.html')
 
+@app.route('/new_flight', methods=['GET', 'POST'])
+def new_airport():
+    """Create new airport"""
+    if request.method == 'POST':
+        if not request.form['airport_id']:
+            flash('airport_id is required', 'error')
+        elif not request.form['airline_id']:
+            flash('airline_id is required', 'error')
+        elif not request.form['airport_code']:
+            flash('airport_code is required', 'error')
+        elif not request.form['city']:
+            flash('city is required', 'error')
+        elif not request.form['state']:
+            flash('state is required', 'error')
+        else:
+            airport = Airport(request.form['airport_id'],request.form['airline_id'],
+                    request.form['airport_code'], request.form['city'],
+                    request.form['state'])
+            db.session.add(airport)
+            db.session.commit()
+            flash(u'Airport successfully created')
+            return redirect(url_for('show_airport'))
+    return render_template('new_airport.html')
+
+@app.route('/new_airline', methods=['GET', 'POST'])
+def new_airline():
+    """Create new airline"""
+    if request.method == 'POST':
+        if not request.form['airline_id']:
+            flash('airline_id is required', 'error')
+        elif not request.form['airline_code']:
+            flash('airline_code is required', 'error')
+        elif not request.form['airline_name']:
+            flash('airline_name is required', 'error')
+        else:
+            airline = Airline(request.form['airline_id'], request.form['airline_code'],
+                    request.form['airline_name'])
+            db.session.add(airline)
+            db.session.commit()
+            flash(u'Airline successfully created')
+            return redirect(url_for('show_airline'))
+    return render_template('new_airline.html')
+
+@app.route('/new_aircraft', methods=['GET', 'POST'])
+def new_aircraft():
+    """Create new airline"""
+    if request.method == 'POST':
+        if not request.form['aircraft_id']:
+            flash('aircraft_id is required', 'error')
+        elif not request.form['airline_id']:
+            flash('airline_id is required', 'error')
+        elif not request.form['aircraft_type']:
+            flash('aircraft_type is required', 'error')
+        elif not request.form['capacity']:
+            flash('capacity is required', 'error')
+        else:
+            aircraft = Aircraft(request.form['aircraft_id'], request.form['airline_id'],
+                    request.form['aircraft_type'], request.form['capacity'])
+            db.session.add(aircraft)
+            db.session.commit()
+            flash(u'Aircraft successfully created')
+            return redirect(url_for('show_aircraft'))
+    return render_template('new_aircraft.html')
+
+
+
+"""=================== EDIT ==================="""
+
+
 @app.route('/edit/<flight_id>', methods=['GET', 'POST'])
 def edit(flight_id):
     flight_edit = Flights.query.get(flight_id)
@@ -76,30 +160,111 @@ def edit(flight_id):
         return redirect(url_for('show_all'))
     return render_template('edit.html', flight_edit=flight_edit)
 
+@app.route('/edit_airport/<airport_id>', methods=['GET', 'POST'])
+def edit_airport(airport_id):
+    """Edit method for editing airport"""
+    airport_edit = Airport.query.get(airport_id)
+    if airport_edit == None:
+        flush('Airport does not exist')
+        return redirect(url_for('show_airport'))
+    elif request.method == 'POST':
+        airport_edit.airport_code = request.form['airport_code']
+        airport_edit.city = request.form['city']
+        airport_edit.state = request.form['state']
+        db.session.commit()
+        flash('Flight successfully edited')
+        return redirect(url_for('show_airport'))
+    return render_template('edit_airport.html', airport_edit=airport_edit)
+
+@app.route('/edit_airline/<airline_id>', methods=['GET', 'POST'])
+def edit_airline(airline_id):
+    """Edit method for editing airline"""
+    airline_edit = Airline.query.get(airline_id)
+    if airline_edit == None:
+        flush('Airline does not exist')
+        return redirect(url_for('show_airline'))
+    elif request.method == 'POST':
+        airline_edit.airline_code = request.form['airline_code']
+        airline_edit.airline_name = request.form['airline_name']
+        db.session.commit()
+        flash('Airline successfully edited')
+        return redirect(url_for('show_airline'))
+    return render_template('edit_airline.html', airline_edit=airline_edit)
+
+@app.route('/edit_aircraft/<aircraft_id>', methods=['GET', 'POST'])
+def edit_aircraft(aircraft_id):
+    """Edit method for editing airline"""
+    aircraft_edit = Aircraft.query.get(aircraft_id)
+    if aircraft_edit == None:
+        flush('Aircraft does not exist')
+        return redirect(url_for('show_aircraft'))
+    elif request.method == 'POST':
+        aircraft_edit.aircraft_type = request.form['aircraft_type']
+        aircraft_edit.capacity = request.form['capacity']
+        db.session.commit()
+        flash('Aircraft successfully edited')
+        return redirect(url_for('show_aircraft'))
+    return render_template('edit_aircraft.html', aircraft_edit=aircraft_edit)
+
+"""=================== DELETE ==================="""
+
 @app.route('/delete/<flight_id>', methods=['GET', 'POST'])
 def delete(flight_id):
-    #delete_flight = Flights.query.filter(Flights.id==flight_id)
-    delete_flight = Flights.query.get(flight_id)
+    """Delete Flihgt"""
+
+    delete_flight = Flight.query.get(flight_id)
     db.session.delete(delete_flight)
     flash('Flight Deleted')
     db.session.commit()
     return redirect(url_for('show_all'))
     
 
+@app.route('/delete_airport/<airport_id>', methods=['GET', 'POST'])
+def delete_airport(airport_id):
+    """Method for deleting airport"""
+    delete_airport = Airport.query.get(airport_id)
+    db.session.delete(delete_airport)
+    flash('Airport deleted!')
+    db.session.commit()
+    return redirect(url_for('show_airport'))
+
+@app.route('/delete_airline/<airline_id>', methods=['GET', 'POST'])
+def delete_airline(airline_id):
+    """Method for deleting airline"""
+    delete_airline = Airline.query.get(airline_id)
+    db.session.delete(delete_airline)
+    flash('Airline deleted!')
+    db.session.commit()
+    return redirect(url_for('show_airline'))
+
+@app.route('/delete_aircraft/<aircraft_id>', methods=['GET', 'POST'])
+def delete_aircraft(airline_id):
+    """Method for deleting aircraft"""
+    delete_aircraft = Aircraft.query.get(aircraft_id)
+    db.session.delete(delete_aircraft)
+    flash('Airline deleted!')
+    db.session.commit()
+    return redirect(url_for('show_aircraft'))
+
 @app.route('/update', methods=['POST'])
 def update_done():
+    """Update All Flight"""
+
     Flight.query.all()
     flash('Updated!')
     db.session.commit()
-    return redirect(url_for('show_all'))
+    return redirect(url_for('show_flight'))
+
+"""=================== LOGIN ==================="""
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != Account.query.filter(Account.login_name==request.form['username']).first():
+        user = Account.query.filter(Account.login_name==request.form['username']).first()
+        if not user:
             error = 'Invalid Username'
-        elif request.form['password'] != Account.query.filter(Account.password==request.form['password']).first():
+        elif request.form['password'] != user.password:
             error = 'Invalid Password'
         else:
             session['logged_in'] = True
